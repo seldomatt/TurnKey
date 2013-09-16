9549c32278d2c8a79ee2971cd53c7b9013289e54
Matthew Salerno <matthewsalern@gmail.com>
2013-09-16 17:09:01 -0400

    WIP #define_method not working with Obj-C #encodeWithCoder

--- lib/turnkey/archiver.rb
+++ lib/turnkey/archiver.rb
@@ -2,10 +2,45 @@ module Turnkey
 
   class Archiver
 
-    def self.define_protocols(instance)
-      instance.class.class_eval do
-        define_method(:initWithCoder) {|decoder| true}
-      end
+
+    def self.archive(instance, key)
+      defineEncodeWithCoder(instance)
+      data = NSKeyedArchiver.archivedDataWithRootObject(instance)
+      p "happens"
+      NSUserDefaults.standardUserDefaults[key.to_s] = data
+    end
+
+    private
+
+    #def self.defineInitWithCoder(instance)
+    #  instance.class.class_eval {
+    #    define_method(:initWithCoder) { |decoder|
+    #      true
+    #    }
+    #  }
+    #end
+
+    #[:@title, :@album, :@artist]
+
+    def self.defineEncodeWithCoder(instance)
+      instance.class.class_eval {
+        include Turnkey::Sanitizers
+        define_method(:encodeWithCoder) { |encoder|
+          instance.instance_variables.each do |prop|
+            reader_sig = reader_sig_for(prop)
+            encoder.encodeObject(instance.send(reader_sig), forKey: reader_sig)
+          end
+        }
+      }
+    end
+
+
+  end
+
+  module Sanitizers
+
+    def reader_sig_for(property)
+      property.to_s.delete("@")
     end
 
   end
--- spec/archiver_spec.rb
+++ spec/archiver_spec.rb
@@ -1,12 +1,40 @@
 describe "Archiver" do
 
-  describe "#define_protocols" do
-    it "should define nskeyedunarchiver protocols on model instance" do
-      song = Song.new(title: "In Bloom", album: "Nevermind", artist: "Nirvana")
-      song.respond_to?(:initWithCoder).should == false
-      Turnkey::Archiver.define_protocols(song)
-      song.respond_to?(:initWithCoder).should == true
+  before do
+    @song = Song.new.tap { |s|
+      s.title = "In Bloom"
+      s.album = "Nevermind"
+      s.artist = "Nirvana"
+    }
+  end
+
+  it "should define nskeyedunarchiver protocols on model instance" do
+    @song.respond_to?(:encodeWithCoder).should == false
+    Turnkey::Archiver.defineEncodeWithCoder(@song)
+    @song.respond_to?(:encodeWithCoder).should == true
+  end
+
+  describe "Archiver#archive" do
+
+    it "should save encoded object to NSUserDefaults under provided key" do
+      Turnkey::Archiver.archive(@song, "nirvana_song")
+      NSUserDefaults.standardUserDefaults["nirvana_song"].should != nil
+
     end
 
   end
+
+
+  #it "#define_protocols should define both ns..archiver methods" do
+  #  #had to make this dummy class as RM was behaving strangely when trying to #remove_method
+  #  #or #undef_method from Song class in setup for this spec
+  #  class NewClass; end
+  #  new_instance = NewClass.new
+  #  new_instance.respond_to?(:initWithCoder).should == false
+  #  new_instance.respond_to?(:encodeWithCoder).should == false
+  #  Turnkey::Archiver.define_protocols(new_instance)
+  #  new_instance.respond_to?(:initWithCoder).should == true
+  #  new_instance.respond_to?(:encodeWithCoder).should == true
+  #end
+
 end
