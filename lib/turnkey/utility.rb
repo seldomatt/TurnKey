af3ca3979dd5afa5c5c6363425dc86801eab7204
tissak <tony@thefrontiergroup.com.au>
2014-02-19 10:49:55 +0800

    We are looking for instance variables then trying to set them via accessors. Not all instance variables have accessors so be defensive. At this point if you are looking to preserve a state you should have an attr_accessor for it.

--- lib/turnkey/utility.rb
+++ lib/turnkey/utility.rb
@@ -3,28 +3,16 @@ module Turnkey
   class Utility
     extend Sanitizers
 
-    #def self.defineProtocols(instance)
-    #  return if instance.respondsToSelector("encodeWithCoder:") && instance.respondsToSelector("initWithCoder:")
-    #  instance.class.class_eval {
-    #    include Turnkey::Proxy
-    #  }
-    #end
-    #
-    #def self.extend_protocols_to_object_references(instance)
-    #  instance.instance_variables.each do |prop|
-    #    value = instance.send(reader_sig_for(prop))
-    #    defineProtocols(value)
-    #  end
-    #end
-
     def self.defineProtocols(instance)
       return if alreadyDefined?(instance)
 
       includeCoderProtocols(instance)
 
       instance.instance_variables.each do |prop|
-        ref = instance.send(reader_sig_for(prop))
-        defineProtocols(ref)
+        if instance.respond_to?(reader_sig_for(prop))
+          ref = instance.send(reader_sig_for(prop))
+          defineProtocols(ref)
+        end
       end
     end
 
